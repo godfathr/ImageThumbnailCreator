bc43c2bdeb68ee4ed9da773cd4fdbedcead0cfa4
MG <mikal.k.grant@outlook.com>
2019-04-07 21:22:09 -0400

    Started unit tests.

---
+++ ImageThumbnailCreator.Tests/JpegThumbnailerUnitTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImageThumbnailCreator.Tests
+{
+    [TestClass]
+    public class JpegThumbnailerUnitTests
+    {
+        private JpegThumbnailer _jpegThumbnailer = new JpegThumbnailer();
+        private string ThumbnailFolder = ConfigurationSettings.AppSettings["TestDirectory"];
+        [TestInitialize]
+        public void Setup()
+        {
+            //TODO: Add any setup steps here
+            _jpegThumbnailer.CheckAndCreateDirectory(ThumbnailFolder);
+        }
+
+        [TestMethod]
+        public void CheckAndCreateDirectory_DirectoryDoesNotExist_SuccessfullyCreatesDirectory()
+        {
+            //setup
+
+            //act            
+            var createdFolder = Directory.Exists(ThumbnailFolder);
+
+            //assert
+
+            Assert.IsTrue(createdFolder);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Directory.Delete(ThumbnailFolder);
+        }
+    }
+}
---
+++ ImageThumbnailCreator.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ImageThumbnailCreator.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ImageThumbnailCreator.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5b51fd20-d784-42fb-8431-680c2da8fa04")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
